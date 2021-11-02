f3eb7e2d04286f846822f195433674f2410e69f8
Marc Backes <hello@marc.dev>
2021-11-02 13:16:36 +0100

    🚀 [feature] added copy to clipboard

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        Made with ❤️ by
+        <a href="https://marc.dev">Marc Backes</a>
+    </footer>
+</template>
+
+<style>
+footer {
+    color: #fff;
+    padding: 0.5rem 1rem;
+    text-align: center;
+}
+
+footer a {
+    color: #fff;
+}
+</style>
